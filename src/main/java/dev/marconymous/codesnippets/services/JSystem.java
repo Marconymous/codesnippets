da7f8e535ccd31f38f1c149ad9718e47602ff6e8
Marc Andri Fuchs <marconymous@outlook.com>
2022-05-04 15:11:52 +0200

    suii

--- src/main/java/dev/marconymous/codesnippets/services/JSystem.java
+++ src/main/java/dev/marconymous/codesnippets/services/JSystem.java
@@ -1,11 +1,27 @@
 package dev.marconymous.codesnippets.services;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
+/**
+ * @author marconymous
+ * @since 2020-05-04
+ * <p>
+ * Services with allround use.
+ */
 @Path("/system")
-public class System {
+public class JSystem {
+
+  /**
+   * Service to test if the server is up and running.
+   *
+   * @return the current time as a json object.
+   */
   @Path("/ping")
+  @GET
+  @Produces("application/json")
   public String ping() {
-    return "pong at " + java.lang.System.currentTimeMillis();
+    return "{\"time\":\"" + System.currentTimeMillis() + "\"}";
   }
 }
